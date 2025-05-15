5608e4d9eaae0c691289f1849385e60aedd2ffa8
jeremie <jeremie-codes>
2025-05-16 00:42:18 +0100

    eeeed

--- resources/views/layouts/master-without-nav.blade.php
+++ resources/views/layouts/master-without-nav.blade.php
@@ -4,13 +4,13 @@
 
 <head>
     <meta charset="utf-8">
-    <title>@yield('title') | {{ env('APP_NAME') }}'</title>
+    <title>@yield('title') | {{ env('APP_NAME') }}</title>
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
     <meta content="Minimal Admin & Dashboard Template" name="description">
     <meta content="Themesdesign" name="author">
     <!-- App favicon -->
-    <link rel="shortcut icon" href="{{ URL::asset('build/images/favicon.ico') }}">
+    <link rel="shortcut icon" href="{{ URL::asset('images/logo.png') }}">
 
     @include('layouts.head-css')
     <!-- Livewire Styles -->
