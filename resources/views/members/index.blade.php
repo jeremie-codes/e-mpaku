8e59496095c083542e7d3dc74f195e0e041fd6eb
jeremie <jeremie-codes>
2025-07-07 00:13:57 +0100

    kekek

--- resources/views/members/index.blade.php
+++ resources/views/members/index.blade.php
@@ -320,14 +320,14 @@
                             </div>
                         </div>
                         <div class="xl:col-span-12">
-                            <label for="employeeId" class="inline-block mb0 text-base font-medium">Référence Member</label>
+                            <label for="employeeId" class="inline-block mb0 text-base font-medium">CIPA</label>
                             <input type="text" id="employeeId" name="ref"
                                 class="form-input border-slate-200 dark:border-zink-500 focus:outline-none focus:border-custom-500 disabled:bg-slate-100 dark:disabled:bg-zink-600 disabled:border-slate-300 dark:disabled:border-zink-500 dark:disabled:text-zink-200 disabled:text-slate-500 dark:text-zink-100 dark:bg-zink-700 dark:focus:border-custom-800 placeholder:text-slate-400 dark:placeholder:text-zink-200"
                                 value="" required>
                         </div>
 
                         <div class="xl:col-span-12">
-                            <label for="employeeId" class="inline-block mb0 text-base font-medium">Référence sécondaire(optionnal)</label>
+                            <label for="employeeId" class="inline-block mb0 text-base font-medium">Référence (optionnal)</label>
                             <input type="text" id="employeeId" name="sec_ref"
                                 class="form-input border-slate-200 dark:border-zink-500 focus:outline-none focus:border-custom-500 disabled:bg-slate-100 dark:disabled:bg-zink-600 disabled:border-slate-300 dark:disabled:border-zink-500 dark:disabled:text-zink-200 disabled:text-slate-500 dark:text-zink-100 dark:bg-zink-700 dark:focus:border-custom-800 placeholder:text-slate-400 dark:placeholder:text-zink-200"
                                 value="" required>
